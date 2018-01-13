f3d59b8c51dddfc1dfa415b4ce07d2d5baf32852
スーイツ <Sweets@users.noreply.github.com>
2018-01-13 15:39:00 -0600

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,45 @@
+
+#include <err.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#include <xcb/xcb.h>
+#include <xcb/xcb_ewmh.h>
+#include <xcb/xcb_xrm.h>
+
+#include <iostream>
+#include <map>
+#include <vector>
+#include <regex>
+
+#include "connection.hpp"
+#include "ewmh.hpp"
+#include "window.hpp"
+#include "workspaces.hpp"
+#include "fifo.hpp"
+#include "custard.hpp"
+
+#include "handlers.cpp"
+#include "connection.cpp"
+#include "ewmh.cpp"
+#include "config.cpp"
+#include "grid.cpp"
+#include "window.cpp"
+#include "workspaces.cpp"
+#include "fifo.cpp"
+#include "custard.cpp"
+
+int main(int argc, char* argv[]) {
+
+    atexit(custard::stop);
+    custard::start();
+
+    return 0;
+
+}
+
