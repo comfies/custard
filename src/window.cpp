3bc7afc841daf7f171fda4fb18811a7db9a3204e
Sweets <coil@hazerd.com>
2018-03-06 20:37:54 -0600

    Fix: focus fuck up

--- src/window.cpp
+++ src/window.cpp
@@ -152,6 +152,14 @@ void Window::focus(void)
         return;
     }
 
+    Window *focused_window = NULL;
+    focused_window = custard::get_focused_window();
+
+    if (focused_window)
+    {
+        focused_window->set_focus_false();
+    }
+
     this->focused = true;
 
     xcb_set_input_focus(
