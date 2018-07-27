f6d755c7aee346b2888824cb676a33ac5fb9ce7d
Sweets <coil@hazerd.com>
2018-07-27 11:39:36 -0500

    Change: focus on last window in group (#13)

--- src/group.c
+++ src/group.c
@@ -98,16 +98,23 @@ focus_group(unsigned int group)
     struct WindowLinkedListElement *element = window_list_head;
     Window *window = NULL;
 
+    xcb_window_t last_window = NULL;
+
     while (element) {
         window = element->window;
 
         if (window_is_in_group(window, group)) {
             map_window(window->id);
+            last_window = window->id;
         } else {
             unmap_window(window->id);
         }
 
         element = element->next;
+    }
+
+    if (last_window) {
+        focus_on_window(window->id);
     }
 
     focused_group = group;
