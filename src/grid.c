6c4107ca3fd6f44911c554bd31c0592165d8f131
Sweets <Sweets@users.noreply.github.com>
2019-06-11 15:01:54 +0000

    Change: grid per monitor

--- src/grid.c
+++ src/grid.c
@@ -1,6 +1,7 @@
 
 #include "custard.h"
 #include "grid.h"
+#include "monitor.h"
 #include "xcb.h"
 
 float grid_x_unit_size_in_pixels = 0;
@@ -11,44 +12,51 @@ unsigned int grid_window_default_y = 0;
 unsigned int grid_window_default_height = 1;
 unsigned int grid_window_default_width = 1;
 
-void apply_configuration_to_grid() {
+void apply_configuration_to_monitor_grid(monitor_t *monitor) {
+
+    if (!monitor)
+        return;
+
+    grid_t *grid = monitor->grid;
 
     unsigned int horizontal_border_sum = (border_total_size * 2) *
         grid_columns;
     unsigned int horizontal_offset_sum = grid_offset_left + grid_offset_right;
     unsigned int horizontal_gap_sum = grid_gap * (grid_columns + 1);
 
-    float usable_x_pixels = (float)(screen->width_in_pixels - (
+    float usable_x_pixels = (float)(monitor->width - (
                 horizontal_border_sum + horizontal_offset_sum +
                     horizontal_gap_sum));
 
-    grid_x_unit_size_in_pixels = usable_x_pixels / (float)grid_columns;
+//    grid_x_unit_size_in_pixels = usable_x_pixels / (float)grid_columns;
+    grid->x_unit_size_in_pixels = usable_x_pixels / (float)grid_columns;
 
     unsigned int vertical_border_sum = (border_total_size * 2) * grid_rows;
     unsigned int vertical_offset_sum = grid_offset_top + grid_offset_bottom;
     unsigned int vertical_gap_sum = grid_gap * (grid_rows + 1);
 
-    float usable_y_pixels = (float)(screen->height_in_pixels - (
+    float usable_y_pixels = (float)(monitor->height - (
                 vertical_border_sum + vertical_offset_sum + vertical_gap_sum));
 
-    grid_y_unit_size_in_pixels = usable_y_pixels / (float)grid_rows;
+//    grid_y_unit_size_in_pixels = usable_y_pixels / (float)grid_rows;
+    grid->y_unit_size_in_pixels = usable_y_pixels / (float)grid_rows;
 
-    grid_window_default_x = (grid_columns / 2) - 1;
+    grid->default_x = (grid_columns / 2) - 1;
 
     if (!(grid_columns % 2)) {
-        grid_window_default_width = 2;
+        grid->default_width = 2;
     } else {
-        grid_window_default_x++;
-        grid_window_default_width--;
+        grid->default_x++;
+        grid->default_width--;
     }
 
-    grid_window_default_y = (grid_rows / 2) - 1;
+    grid->default_y = (grid_rows / 2) - 1;
 
     if (!(grid_rows % 2)) {
-        grid_window_default_height = 2;
+        grid->default_height = 2;
     } else {
-        grid_window_default_y++;
-        grid_window_default_height--;
+        grid->default_y++;
+        grid->default_height--;
     }
 
 }
@@ -64,17 +72,25 @@ float grid_get_offset(float unit_size, unsigned int amount) {
 }
 
 float grid_get_span_x(unsigned int amount) {
-    return grid_get_span(grid_x_unit_size_in_pixels, amount);
+    monitor_t *focused_monitor = get_focused_monitor();
+    return grid_get_span(focused_monitor->grid->x_unit_size_in_pixels, amount);
 }
 
 float grid_get_span_y(unsigned int amount) {
-    return grid_get_span(grid_y_unit_size_in_pixels, amount);
+    monitor_t *focused_monitor = get_focused_monitor();
+    return grid_get_span(focused_monitor->grid->y_unit_size_in_pixels, amount);
 }
 
 float grid_get_x_offset(unsigned int amount) {
-    return grid_get_offset(grid_x_unit_size_in_pixels, amount);
+    monitor_t *focused_monitor = get_focused_monitor();
+    return grid_get_offset(
+            focused_monitor->grid->x_unit_size_in_pixels, amount) +
+        focused_monitor->x;
 }
 
 float grid_get_y_offset(unsigned int amount) {
-    return grid_get_offset(grid_y_unit_size_in_pixels, amount);
+    monitor_t *focused_monitor = get_focused_monitor();
+    return grid_get_offset(
+            focused_monitor->grid->y_unit_size_in_pixels, amount) +
+        focused_monitor->y;
 }
--- src/grid.h
+++ src/grid.h
@@ -1,6 +1,20 @@
 #ifndef GRID_H
 #define GRID_H
 
+
+typedef struct {
+    unsigned int default_x;
+    unsigned int default_y;
+    unsigned int default_width;
+    unsigned int default_height;
+
+    float x_unit_size_in_pixels;
+    float y_unit_size_in_pixels;
+} grid_t;
+
+#include "monitor.h"
+
+/*
 float grid_x_unit_size_in_pixels;
 float grid_y_unit_size_in_pixels;
 
@@ -8,8 +22,8 @@ extern unsigned int grid_window_default_x;
 extern unsigned int grid_window_default_y;
 extern unsigned int grid_window_default_height;
 extern unsigned int grid_window_default_width;
-
-void apply_configuration_to_grid(void);
+*/
+void apply_configuration_to_monitor_grid(monitor_t *);
 
 float grid_get_span(float, unsigned int);
 float grid_get_offset(float, unsigned int);
