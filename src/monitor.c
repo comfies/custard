237287bec600d6c985737c5880a0b0eb4a06e157
Sweets <Sweets@users.noreply.github.com>
2019-06-11 15:02:50 +0000

    More monitor work

--- src/monitor.c
+++ src/monitor.c
@@ -2,7 +2,9 @@
 #include <xcb/randr.h>
 
 #include "custard.h"
+#include "grid.h"
 #include "monitor.h"
+#include "vector.h"
 #include "xcb.h"
 
 unsigned short initialize_monitors() {
@@ -14,7 +16,7 @@ unsigned short initialize_monitors() {
     screen_resources = xcb_randr_get_screen_resources_reply(xcb_connection,
             xcb_randr_get_screen_resources(xcb_connection, screen->root), NULL);
 
-    xcb_randr_output_t *monitors = xcb_randr_get_screen_resources_outputs(
+    xcb_randr_output_t *outputs = xcb_randr_get_screen_resources_outputs(
             screen_resources);
 
     unsigned int number_of_monitors;
@@ -24,9 +26,10 @@ unsigned short initialize_monitors() {
     xcb_randr_get_output_info_reply_t *output_data;
     xcb_randr_get_crtc_info_reply_t *monitor_data;
 
+    monitor_t *monitor = NULL;
     for (unsigned int index = 0; index < number_of_monitors; index++) {
         output_data = xcb_randr_get_output_info_reply(xcb_connection,
-                xcb_randr_get_output_info(xcb_connection, monitors[index],
+                xcb_randr_get_output_info(xcb_connection, outputs[index],
                     XCB_CURRENT_TIME), NULL);
 
         if (!output_data || !output_data->crtc) {
@@ -42,6 +45,18 @@ unsigned short initialize_monitors() {
                 monitor_data->x, monitor_data->y,
                 monitor_data->width, monitor_data->height);
 
+        monitor = (monitor_t *)malloc(sizeof(monitor_t));
+        monitor->x = monitor_data->x;
+        monitor->y = monitor_data->y;
+        monitor->height = monitor_data->height;
+        monitor->width = monitor_data->width;
+
+        monitor->grid = (grid_t *)malloc(sizeof(grid_t));
+        // each monitor has its own grid
+        apply_configuration_to_monitor_grid(monitor);
+
+        push_to_vector(monitors, monitor);
+
         free(monitor_data);
         free(output_data);
     }
@@ -49,3 +64,30 @@ unsigned short initialize_monitors() {
     return 1;
 }
 
+monitor_t *get_focused_monitor() {
+    // The focused monitor is the one with the cursor on it.
+    // Not much more complex than that.
+    
+    xcb_query_pointer_reply_t *pointer = xcb_query_pointer_reply(xcb_connection,
+            xcb_query_pointer(xcb_connection, screen->root), NULL);
+
+    unsigned int cursor_x = (unsigned int)pointer->root_x;
+    unsigned int cursor_y = (unsigned int)pointer->root_y;
+
+    unsigned int summation_x = 0;
+    unsigned int summation_y = 0;
+
+    monitor_t *monitor = NULL;
+    for (unsigned int index = 0; index < monitors->size; index++) {
+        monitor = get_from_vector(monitors, index);
+
+        if (cursor_x < (summation_x + monitor->width) &&
+                cursor_y < (summation_y + monitor->height))
+            return monitor;
+
+        summation_x += monitor->width;
+        summation_y += monitor->height;
+    }
+
+    return NULL;
+}
--- src/monitor.h
+++ src/monitor.h
@@ -1,6 +1,18 @@
 #ifndef MONITOR_H
 #define MONITOR_H
 
+#include "grid.h"
+
+typedef struct {
+    unsigned int x;
+    unsigned int y;
+    unsigned int height;
+    unsigned int width;
+
+    grid_t *grid;
+} monitor_t;
+
 unsigned short initialize_monitors(void);
+monitor_t *get_focused_monitor(void);
 
 #endif
