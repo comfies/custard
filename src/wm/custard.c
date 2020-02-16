678c9f465958543c03fdf6b150569d8b8f05a054
Sweets <Sweets@users.noreply.github.com>
2020-02-16 15:36:32 -0800

    Add: loglevels

--- src/wm/custard.c
+++ src/wm/custard.c
@@ -15,25 +15,27 @@
 #include "../vector.h"
 
 #include "../ipc/ipc.h"
+#include "../ipc/parsing.h"
 #include "../ipc/socket.h"
 #include "../xcb/connection.h"
 #include "../xcb/ewmh.h"
 #include "../xcb/xrandr.h"
 // TODO: workspaces need reimplementation
 
-char* rc_path = NULL;
+char *rc_path = NULL;
+unsigned short loglevel = 0;
 unsigned short custard_is_running = 0;
 
-int custard(int argc, char** argv) {
+int custard(int argc, char **argv) {
     /* Process arguments */
 
-    char* argument;
+    char *argument;
     for (int index = 1; index < argc; index++) {
         argument = argv[index];
 
         if (!strcmp(argument, "--rc")) {
             if (!argv[index + 1] || !strlen(argv[index + 1])) {
-                log("No rc specified, exiting.");
+                log_fatal("No rc specified with --rc, exiting.");
                 return EXIT_FAILURE;
             }
 
@@ -45,9 +47,20 @@ int custard(int argc, char** argv) {
                 index++;
                 continue;
             } else {
-                log("Specified rc is not executable, exiting.");
+                log_fatal("Specified rc is not executable, exiting.");
                 return EXIT_FAILURE;
             }
+        }
+
+        if (!strcmp(argument, "--loglevel")) {
+            if (!argv[index + 1] || !strlen(argv[index + 1])) {
+                log_fatal("No loglevel specified with --loglevel, exiting.");
+                return EXIT_FAILURE;
+            }
+
+            loglevel = (short)string_to_integer(argv[index + 1]);
+            index++;
+            continue;
         }
     }
 
@@ -118,49 +131,54 @@ unsigned short initialize() {
 void finalize() {
     /* Free used memory for windows */
     if (windows) {
-        window_t* window;
+        window_t *window;
         while ((window = vector_iterator(windows)))
             unmanage_window(window->id);
         deconstruct_vector(windows);
     }
 
-    kv_pair_t* kv_pair;
-    monitor_t* monitor;
-    labeled_grid_geometry_t* labeled_geometry;
-    while ((monitor = vector_iterator(monitors))) {
-        free(monitor->name);
-        free(monitor->geometry);
+    kv_pair_t *kv_pair;
+    monitor_t *monitor;
+    labeled_grid_geometry_t *labeled_geometry;
+    if (monitors) {
+        while ((monitor = vector_iterator(monitors))) {
+            free(monitor->name);
+            free(monitor->geometry);
 
-        if (monitor->geometries) {
-            while ((labeled_geometry = vector_iterator(monitor->geometries))) {
-                free(labeled_geometry->label);
-                free(labeled_geometry->geometry);
-                free(labeled_geometry);
+            if (monitor->geometries) {
+                while ((labeled_geometry = vector_iterator(
+                    monitor->geometries))) {
+                    free(labeled_geometry->label);
+                    free(labeled_geometry->geometry);
+                    free(labeled_geometry);
+                }
+                deconstruct_vector(monitor->geometries);
             }
-            deconstruct_vector(monitor->geometries);
-        }
 
-        if (monitor->configuration) {
-            while ((kv_pair = vector_iterator(monitor->configuration))) {
-                free(kv_pair->key);
-                free(kv_pair->value);
-                free(kv_pair);
+            if (monitor->configuration) {
+                while ((kv_pair = vector_iterator(monitor->configuration))) {
+                    free(kv_pair->key);
+                    free(kv_pair->value);
+                    free(kv_pair);
+                }
+                deconstruct_vector(monitor->configuration);
             }
-            deconstruct_vector(monitor->configuration);
         }
     }
 
-    while ((kv_pair = vector_iterator(configuration))) {
-        free(kv_pair->key);
-        free(kv_pair->value);
-        free(kv_pair);
+    if (configuration) {
+        while ((kv_pair = vector_iterator(configuration))) {
+            free(kv_pair->key);
+            free(kv_pair->value);
+            free(kv_pair);
+        }
+        deconstruct_vector(configuration);
     }
-    deconstruct_vector(configuration);
 
     /* Free rules, if any */
 
     if (rules) {
-        rule_t* rule;
+        rule_t *rule;
         while ((rule = vector_iterator(rules))) {
             free(rule->expression);
 
@@ -181,10 +199,11 @@ void finalize() {
     finalize_socket();
 }
 
-void _log(const char* file, const char* function, const int line,
-    char* formatting, ...) {
+void _log(unsigned short level, const char *file, const char *function,
+    const int line, char *formatting, ...) {
 
-    fprintf(stderr, "%s:%s L%d: ", file, function, line);
+    if (level >= loglevel)
+        fprintf(stderr, "%s:%s L%d: ", file, function, line);
 
     va_list ap;
     va_start(ap, formatting);
