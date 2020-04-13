eefb1531990108036d10a9c1541b509393a8c115
Sweets <Sweets@users.noreply.github.com>
2020-04-12 18:30:00 -0700

    Fix: windows not raising and slight refactor

--- src/wm/handlers.c
+++ src/wm/handlers.c
@@ -116,40 +116,41 @@ void handle_window_click(xcb_generic_event_t *generic_event) {
     event = (xcb_button_press_event_t*)generic_event;
 
     xcb_window_t window_id = event->event;
+    xcb_window_t parent_id = XCB_WINDOW_NONE;
+    xcb_window_t previous_window = focused_window;
 
     window_t *window;
     if (windows) {
         while ((window = vector_iterator(windows))) {
             if (window->parent == window_id) {
-                reset_vector_iterator(windows);
+                parent_id = window_id;
                 window_id = window->id;
+
+                reset_vector_iterator(windows);
                 break;
             }
         }
-    }// redirect the parent to the child
+    }
 
     log_debug("Window(%08x) clicked", window_id);
 
-    raise_window(window_id);
-    if (window_is_managed(window_id))
-        focus_on_window(get_window_by_id(window_id));
-    else {
-        xcb_window_t previous_window = focused_window;
+    if (window_is_managed(window_id)) {
+        raise_window(parent_id);
+        focus_on_window(window);
+    } else {
         focused_window = window_id;
 
         if (window_is_managed(previous_window)) {
             xcb_grab_button(xcb_connection, 0, previous_window,
                 XCB_EVENT_MASK_BUTTON_PRESS,
                 XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC,
-                XCB_NONE, XCB_NONE,
-                XCB_BUTTON_INDEX_ANY, XCB_MOD_MASK_ANY);
+                XCB_NONE, XCB_NONE, XCB_BUTTON_INDEX_ANY, XCB_MOD_MASK_ANY);
             decorate(get_window_by_id(previous_window));
         }
 
+        raise_window(window_id);
         focus_window(window_id);
     }
-
-    apply();
 }
 
 /* Signal handlers */
