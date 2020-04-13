0feb57733e29b562e14449f1afe2cb40e8a61b95
Sweets <Sweets@users.noreply.github.com>
2020-04-12 19:25:10 -0700

    Change: focus_on_window() obsoleted

--- src/wm/custard.c
+++ src/wm/custard.c
@@ -150,12 +150,25 @@ void manage_pre_existing_windows() {
         if (window_should_be_managed(child)) {
             window = manage_window(child);
             map_window(child);
-            raise_window(window->parent);
-            focus_on_window(window);
+            xcb_grab_button(xcb_connection, 0, child,
+                XCB_EVENT_MASK_BUTTON_PRESS,
+                XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC,
+                XCB_NONE, XCB_NONE,  XCB_BUTTON_INDEX_ANY, XCB_MOD_MASK_ANY);
             decorate(window);
             log_debug("Pre-existing window(%08x) managed",
                 child, window->workspace);
         }
+
+    }
+
+    if (window) {
+    // Raise and focus the last window
+        focused_window = window->id;
+        xcb_ungrab_button(xcb_connection,
+            XCB_BUTTON_INDEX_ANY, focused_window, XCB_MOD_MASK_ANY);
+        raise_window(window->parent);
+        focus_window(window->id);
+        decorate(window);
     }
 
     apply();
